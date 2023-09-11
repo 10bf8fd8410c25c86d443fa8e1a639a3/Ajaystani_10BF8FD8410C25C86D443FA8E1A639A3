440bb983dee38f8983b99de54a942893eb24fcb1
10bf8fd8410c25c86d443fa8e1a639a3 <144227631+10bf8fd8410c25c86d443fa8e1a639a3@users.noreply.github.com>
2023-09-11 21:19:03 +0530

    challenge 1.1

--- challenge unit 1/main.py
+++ challenge unit 1/main.py
@@ -1,4 +1,16 @@
-def Hello world():
-print("Hello world")
-
-Hello world()
+# python 3 program to find
+# factorial of given number def factorial(n):
+if n<0:
+  return 0
+elif n==0 or n==1:
+  return 1
+else:
+  fact=1
+  while(n>1):
+    fact*=n
+    n-=1
+    return fact
+    #driver code
+num=5
+print("factorial of",num",is",factorial(num))
+#this code is contributed by dharmik thakkar
